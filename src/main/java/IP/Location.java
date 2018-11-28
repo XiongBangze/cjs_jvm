aa095d6a9b6413ecb78e0b209b8914ae16d15ccd
bangze.xiong <Aa123456>
2018-11-28 16:21:47 +0800

    ip纯真库

---
+++ src/main/java/IP/Location.java
@@ -0,0 +1,24 @@
+package IP;
+
+public class Location {
+
+   String country;
+   String area;
+
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+}
